2250d26c12d96daa8244ba5d4ab68911c141b74f
joel-adebayo <44178013+joel-adebayo@users.noreply.github.com>
2021-04-12 12:08:21 -0500

    Update API Request.py

--- API Request.py
+++ API Request.py
@@ -17,9 +17,9 @@ import requests_cache #For caching api results
 import sys
 
 #Send a GET request to tomtom api
-def get_address (urlInfo): 
+def get_address (url): 
     try:
-        r = requests.get(urlInfo,params)
+        r = requests.get(url,params)
         geocodeJson = r.json()
         return geocodeJson['addresses'][0]['address']
         
@@ -30,20 +30,21 @@ if __name__ == '__main__':
 
     #Specify parameters for api request
     params = {
-        'key': 'UNTDKBMLdLaqZJogsq53pMx3M1AM5Z43',
+        'key': 'shkEPTAKIwt6G92CdZAUEuw8U9pWbwfZ',
     }
 
     #Use computer memory for api caching
     requests_cache.install_cache(backend='memory')
 
-    #Read input excel file and create a list of urls
+    #Read input CSV file and create a list of urls
     read_df = pd.read_excel(sys.argv[1], engine='openpyxl')
+
     urlList = [(f'https://api.tomtom.com/search/2/reverseGeocode/{read_df.loc[x,["Latitude"]].values[0]},'
                 f'{read_df.loc[x,["Longitude"]].values[0]}.JSON') for x in range(len(read_df.index))]
 
 
     #Use multithreading to excute the requests in parellel.
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         results = executor.map(get_address,urlList)
 
     data = [f'{val["streetNumber"]} {val["streetName"]}, {val["municipality"]} {val["postalCode"]}'\
